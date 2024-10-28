58b6a02469a3fe643672af232bdf3be04bc6ee6d
femas arianda <femasariandarizki@students.undip.ac.id>
2024-10-29 06:36:09 +0700

    upload img - placeholder II

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,20 +2,23 @@
 import React from "react";
 import "./index.css";
 
+const placeholderImage =
+    "https://github.com/FemasAriandaRizki/PrakPPB_modul6_tugas/blob/eabf8cf55d8880c8df732c786f133b701a5ce6f8/src/images/image%20-%20placeholder.jpg"; //
+
 export default function index({ data, onClick }) {
     return (
         <div className="card" onClick={onClick}>
             {data ? (
                 <>
                     <figure>
-                        {data.i && data.i.imageUrl ? (
-                            <img
-                                src={data.i.imageUrl}
-                                alt={data.l || "No Title"}
-                            />
-                        ) : (
-                            <p>No Image Available</p>
-                        )}
+                        <img
+                            src={
+                                data.i && data.i.imageUrl
+                                    ? data.i.imageUrl
+                                    : placeholderImage
+                            }
+                            alt={data.l || "No Title"}
+                        />
                     </figure>
                     <div className="card-info">
                         <h3>{data.l || "No Title Available"}</h3>
